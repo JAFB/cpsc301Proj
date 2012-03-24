f847ce19103545414b85e2741438fc23431c8696
Jun <junbiaocanada@gmail.com>
2012-03-24 13:11:56 -0600

    clean up

--- SourceCode/ProjSRC/startServer.js
+++
@@ -1,8 +0,0 @@
-require("./config.js")
-var server = require("./HttpServer/simpleServer.js");
-var expressServer = require("./HttpServer/expressServer.js")
-
-/*
-To Launch the server
- */
-expressServer.launchExpressServer(13910);
