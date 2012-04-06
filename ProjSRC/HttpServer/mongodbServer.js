e3e220a5a136c1fa5ff4b59d9063fd39dec80389
Akio Hoshikawa <haria02@hotmail.com>
2012-04-05 19:13:35 -0600

    merge IM function

--- ProjSRC/HttpServer/mongodbServer.js
+++ ProjSRC/HttpServer/mongodbServer.js
@@ -1,12 +1,13 @@
+/*
+	database related action handler
+ */
+
 var datamodule = require('../Modules/datamodule.js')
 var Database = require('../Modules/database.js').Database;
 var mongodbObj = new Database('cem_db', 'localhost', 27017, function(err){
     console.log("Database connection error : " + err);
 });
 
-/*
-
- */
 exports.findAll = function(collectionName, request, response){
     mongodbObj.findAll(collectionName, function(err, data){
         response.contentType('json');
@@ -98,31 +99,164 @@ exports.remove = function(collectionName, request, response){
     })
 };
 
+
+/* Login request handler */
 exports.userLogin = function(collectionName,request,response){
 
-    var loginQuery = {email: request.body['userid']} // to user email as login id.
+    var loginQuery = {email: request.body['userid']} //user email as login id.
 
 	mongodbObj.findOne(collectionName,loginQuery, function(err, docs){
 		response.contentType('json');
 		if (err){//Database Error
 			console.log(err);
-			request.send("500 Internal Server Error",500);
-		}else if(docs==null){
+		}else if(docs==null || request.body['password']!==docs.password){
+			/* if no such ID, or pasword does not match */
 			response.json({
 				failure: true
 			});
-		}else if(request.body['password']===docs.password){
+		}else{//Login success
+			/* Give a session */
 			request.session.auth = true;
 			request.session.username = docs.name;
 			request.session.admin = docs.admin;
-            request.session.email = docs.email;
+			request.session.email = docs.email;
 			response.json({
 				success: true
 			});
-		}else{
-			response.json({
-				failure: true
-			});
 		}
 	});
+};
+
+/* Recent Instant Message loading */
+exports.IMLoad = function(collectionName, request, response){
+    mongodbObj.findOne(collectionName, {timeSlot:"resent"} , function(err, data){
+        if (err) {
+            console.log(err)
+        } else {
+			if(data==null){//recent message is empty
+				response.json({
+					success:true,
+					data: { mssgForm: "" }
+				})
+			}
+			else{
+				updateMssgDate(collectionName, request, response, data);
+			}
+        }
+    })
+};
+/* Load last day instant message */
+exports.IMLoadLastDay = function(collectionName, request, response){
+    mongodbObj.findOne(collectionName, {timeSlot:"lastday"} , function(err, data){
+        if (err) {
+            console.log(err)
+        } else {
+			if(data==null){//if there is no message
+				response.json({
+					success:true,
+					data: { mssgForm: "" }
+				})
+			}
+			else{
+				response.json({
+					success:true,
+					data: data
+				})
+			}
+        }
+    })
+};
+/* Check the time of messages and change timeSlot if nessesary */
+function updateMssgDate(collectionName, request, response, data){
+	var oneday =  1000 * 60 * 60 * 24;
+	if(( new Date() - data.time.getTime() ) > oneday){//out of date
+		var currentData = data;
+		/* make recent messages lastday */
+		mongodbObj.findOne(collectionName, {timeSlot:"lastday"} , function(err, data){
+			currentData.timeSlot = "lastday";
+			delete currentData._id;
+			if(data==null){//no lastday record exists
+				mongodbObj.insert(collectionName, currentData, function(err, data){
+					if (err) {
+						console.log("error from inserting data!!")
+						console.log(err);
+					} 
+				});
+			}else{//overwrite current lastday record
+				mongodbObj.update(collectionName,data._id.toHexString(), currentData, function(err, data){
+					if (err) {
+						console.log("error from inserting data!!")
+						console.log(err);
+					} 
+				});
+			}
+		});
+		/* make recent record empty */
+		var now = new Date();
+		var time = new Date(now.getFullYear(),now.getMonth(),now.getDate(),0);
+		mongodbObj.update(collectionName,data._id.toHexString(), {mssgForm:"",time: time }, function(err, data){
+			if (err) {
+				console.log("error from inserting data!!")
+				console.log(err);
+			} 
+		});
+		/* Return empty data */
+		response.json({
+				success: true,
+				data: { mssgForm: "" }
+		})
+	}else{//not out of date
+		response.json({
+			success:true,
+			data: data
+		})
+	}
+};
+/* Save message in database */
+exports.IMSave = function(collectionName, request, response){
+	/* Create object */
+	var now = new Date();
+	var IMData = {
+		timeSlot : "resent",
+		time : new Date(now.getFullYear(),now.getMonth(),now.getDate(),0),
+		mssgForm : request.param("mssgForm")
+	}
+    mongodbObj.findOne(collectionName, {timeSlot:"resent"} , function(err, data){
+        if (err) {
+            console.log(err)
+        } else {
+			if(data==null){//if there is no recent record
+				mongodbObj.insert(collectionName, IMData, function(err, data){
+					if (err) {
+						console.log("error from inserting data!!")
+						console.log(err);
+					} else {
+						response.json({
+							success: true
+						})
+					}
+				});
+			}else{//recent record exists
+				/* Build new message object */
+				var mssgData
+				if(data.mssgForm=="")
+					mssgData = IMData.mssgForm;
+				else
+					mssgData = data.mssgForm +'\n'+ IMData.mssgForm;
+				/* Overwrite record */
+				mongodbObj.update(collectionName,
+					data._id.toHexString(),
+					{mssgForm : mssgData, time: IMData.time} , function(err){
+						if (err) {
+							console.log("error from updating data!!")
+							console.log(err);
+						} else {
+							response.json({
+								success: true,
+							})
+						}
+					});
+			}
+        }
+    })
 };
